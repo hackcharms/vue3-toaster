fb55319ee14ead8b607fc46b2d59b15c6f4128f9
hackcharms <zaa78692@gmail.com>
2023-11-18 16:07:45 +0530

    removed vuetify added tailwind and related config

--- playground/components/TheController.vue
+++ playground/components/TheController.vue
@@ -1,83 +1,132 @@
+<script setup lang="ts">
+import { watch, ref, withDefaults, defineProps, reactive } from "vue";
+import {
+  ToastContainer,
+  useToasterConfig,
+  defaultConfig,
+  ToastVariant,
+  ToastContainerConfig,
+  useToaster,
+} from "vue3-toaster";
+const { theme, ...config } = defaultConfig;
+const controller = reactive<Omit<ToastContainerConfig, "theme">>(
+  Object.assign({}, config)
+);
+const toasterData = reactive({
+  type: "info" as ToastVariant,
+  title: "Information",
+  text: "Hey This is an Information",
+});
+
+const reset = () => {
+  Object.assign(controller, config);
+};
+const setGlobalConfig = () => {
+  useToasterConfig().update({ ...controller });
+};
+const toast = () => {
+  useToaster().add({ ...toasterData, options: { ...controller } });
+};
+</script>
+
 <template>
-  <div class="pa-4 ma-4 border text-white">
-    <h4>TheController</h4>
-    <v-form>
-      <v-row>
-        <v-col cols="6">
-          <v-slider
-            v-model="formData.duration"
-            :label="`Duration ${formData.duration}`"
-            :min="1"
-            color="primary"
-            max="100"
-            step="1"
-          >
-          </v-slider>
-        </v-col>
-        <v-col cols="6">
-          <v-switch
-            color="primary"
-            v-model="formData.closable"
-            label="closable"
-            required
-          ></v-switch>
-        </v-col>
-        <v-col cols="6">
-          <v-switch
-            v-model="formData.closeOnDoubleClick"
-            color="primary"
-            label="Close On Double Click"
-            required
-          ></v-switch>
-        </v-col>
-        <v-col cols="6">
-          <v-switch
-            v-model="formData.pauseOnHover"
-            color="primary"
-            :label="`Pause On Hover`"
-            required
-          ></v-switch>
-        </v-col>
-        <v-col cols="12" class="text-center">
-          <!-- <v-btn color="success" @click="emitChange" class="mr-4">
-            Apply Default
-          </v-btn> -->
-          <v-btn color="error" class="mr-4" @click="reset">
-            Reset Setting
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-form>
+  <div>
+    <div class="grid grid-cols-2 gap-4 mb-4">
+      <div class="flex items-center">
+        <input v-model="controller.duration" type="range" />
+        <label class="ms-2">duration: {{ controller.duration }}s</label>
+      </div>
+      <div class="flex items-center">
+        <input
+          v-model="controller.closable"
+          id="closable"
+          type="checkbox"
+          class="checkbox"
+        />
+        <label for="closable" class="ms-2"
+          >closable : {{ controller.closable }}</label
+        >
+      </div>
+      <div class="flex items-center">
+        <input
+          v-model="controller.pauseOnHover"
+          id="pauseOnHover"
+          type="checkbox"
+          value=""
+          class="checkbox"
+        />
+        <label for="pauseOnHover" class="ms-2"
+          >pauseOnHover: {{ controller.pauseOnHover }}</label
+        >
+      </div>
+      <div class="flex items-center">
+        <input
+          v-model="controller.closeOnDoubleClick"
+          id="closeOnDoubleClick"
+          type="checkbox"
+          value=""
+          class="checkbox"
+        />
+        <label for="closeOnDoubleClick" class="ms-2"
+          >closeOnDoubleClick : {{ controller.closeOnDoubleClick }}</label
+        >
+      </div>
+      <div>
+        <label
+          class="block mb-2 pl-2 text-sm font-medium text-gray-900 dark:text-white"
+          >Select Toaster Type</label
+        >
+        <select
+          v-model="toasterData.type"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option disabled value="">Please select one</option>
+          <option value="info">Info</option>
+          <option value="success">Success</option>
+          <option value="warn">Warning</option>
+          <option value="error">Error</option>
+        </select>
+      </div>
+      <div class="col-span-2">
+        <h1 class="text-lg text-center w-full">Title</h1>
+        <input
+          v-model="toasterData.title"
+          type="text"
+          class="input-text"
+          placeholder="Toaster title here"
+          required
+        />
+      </div>
+      <div class="col-span-2">
+        <h1 class="text-lg text-center w-full">Text</h1>
+        <textarea
+          v-model="toasterData.text"
+          class="input-text"
+          placeholder="Toaster text here"
+          required
+        />
+      </div>
+      <div class="col-span-2 text-center">
+        <button class="btn btn-primary mr-3" type="button" @click="toast">
+          Fire Toast
+        </button>
+        <button
+          class="btn btn-primary mr-3"
+          type="button"
+          @click="setGlobalConfig"
+        >
+          Update global config
+        </button>
+        <button class="btn btn-error" type="button" @click="reset">
+          Reset config
+        </button>
+      </div>
+    </div>
+    <pre class="text-black dark:text-gray-200">
+      {{ controller }},
+      {{ toasterData }}
+      {{ useToasterConfig().all }}
+      </pre
+    >
   </div>
 </template>
-<script lang="ts" setup>
-import { reactive } from "vue";
-import { ToastContainerConfig } from "../../src";
-import { defaultConfig } from "../../src/utils";
-import { watch } from "vue";
-
-const $props = withDefaults(defineProps<ToastContainerConfig>(), {
-  duration: 10,
-});
-const formData = reactive(Object.assign({}, { ...$props }));
-const reset = () => {
-  Object.assign(formData, defaultConfig);
-};
-const emitChange = () => {
-  emit("apply", Object.assign({}, formData));
-};
-watch(
-  () => formData,
-  (value) => {
-    console.log("changed", value);
-    emitChange();
-  },
-  {
-    deep: true,
-  }
-);
-const emit = defineEmits<{
-  apply: [options: ToastContainerConfig];
-}>();
-</script>
-<style lang="scss" scoped></style>
--- playground/main.ts
+++ playground/main.ts
@@ -2,18 +2,8 @@ import { createApp } from "vue";
 import "./style.css";
 import ToastPlugin from "vue3-toaster";
 // import ToastPlugin from "../src";
-import { createVuetify } from "vuetify";
-import "vuetify/styles";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
-
-const vuetify = createVuetify({
-  components,
-  directives,
-});
 import App from "./App.vue";
 createApp(App)
-  .use(vuetify)
   .use(ToastPlugin, {
     closable: false,
   })
