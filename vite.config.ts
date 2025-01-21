53ee445560b4cc4970aec81eb6f7ea273f3e9329
hackcharms <hackcharms@gmail.com>
2025-01-21 18:06:56 +0530

    removing the playground set up as it is moved into docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
 import { defineConfig } from 'vite'
-import vue from "@vitejs/plugin-vue";
+import vue from '@vitejs/plugin-vue'
+import path from "path";
+import dts from "vite-plugin-dts";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  root: "playground",
-  base: "/vue3-toaster/",
+  plugins: [dts(), vue()],
+  build: {
+    lib: {
+      entry: path.resolve("./src/index.ts"),
+      name: "vue3Toaster",
+      formats: ["es", "umd"],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue","docs/*"],
+      output: {
+        exports: "named",
+        globals: {
+          vue: "Vue",
+      },
+      },
+    },
+  
+    sourcemap: true,
+    emptyOutDir: true,
+  },
 });
